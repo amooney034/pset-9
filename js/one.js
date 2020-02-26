287aa1eeaae78a9c04ab6e95607df2ee75258da6
amooney034 <55448881+amooney034@users.noreply.github.com>
2020-02-26 08:01:38 -0500

    Update one.js

--- js/one.js
+++ js/one.js
@@ -1,13 +1,48 @@
-var canvas = doucmnet.createElement("canvas");
+var canvas = document.createElement("canvas");
 document.body.appendChild(canvas);
 var ctx = canvas.getContext(2d);
 
+var grid = [];
+
 var height;
 var width;
+var margin; 
 setDimensions();
 
+const GRID_CIRCLE = 0.7;
+const GRID_COLS = 7;
+const GRID_ROWS = 6;
+const MARGIN = 0.02;
+const COLOR_BACKGROUND = "blue";
+const COLOR_COMP = "red";
+const COLOR_COMP_DRK = "darkred";
+const COLOR_FRAME = "blue";
+const 
+class Cell {
+  constructor(left, top, w, h, row, col) {
+    this.bot = top + h;
+    this.left = left;
+    this.right = left + w;
+    this.top = top;
+    this.w = w;
+    this.h = h;
+    this.row = row;
+    this.col = col;
+    this.cx = left + w / 2;
+    this.cy = left + h / 2;
+    this.r = w * GRID_CIRCLE / 2;
+    this.owner = null;
+  }
+  draw(/** @type {CanvasRenderingConetext2D} */ ctx) {
+    let color = this.owner == null ? COLOR_BACKGROUND : this.owner ? COLOR_PLAY : COLOR_COMP;
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.arc(this.cx, this.cy, this.r, 0, Math.PI * 2);
+    ctx.fill();
+  }
+}
+
 window.addEventListener("resize", setDimensions);
-const COLOR_BACKGROUND = "blue"
 var timeDif;
 var timeLast;
 requestAnimationFrame(loop);
@@ -25,9 +60,31 @@ function loop(timeNow) {
 }
 
 function createBoard() {
+  grid = [];
   
+  let cell, marginX, marginY;
+  
+  if ((width - margin * 2) * GRID_ROWS / GRID_COLS < height - margin * 2) {
+    cell = (width - margin * 2) / GRID_COLS;
+    marginX = margin;
+    marginY = (height - cell * GRID_ROWS) / 2;
 }
 
+  else {
+    cell = (height - margin * 2) / GRID_ROWS);
+    marginX = (width - cell * GRID_COLS) / 2;
+    marginY = margin; 
+  }
+  for (let i = 0; i < GRID_ROWS; i++) {
+    grid[i] = [];
+    for (let j = 0; j < GRID_COLS; j++) {
+      let left = marginX + j * cell;
+      let top = marginY + i * cell;
+      grid [i][j] = new Cell(left, top, cell, cell, i, j);
+    }
+}
+}
+  
 function drawBoard() {
   ctx.fillStyle = COLOR_BACKGROUND;
   ctx.fillRect(0, 0, width, height);
@@ -42,5 +99,6 @@ height = window.innerHeight;
 width = window.innerWidth;
 canvas.height = height;
 canvas.width = width;
+margin = MARGIN * Math.min(height, width);
 newGame();
 }
